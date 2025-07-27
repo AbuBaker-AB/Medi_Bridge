b3b01eda3064fd73b7af33c97df0bebaf0a37764
aktheraqila <aktheraqila@gmail.com>
2025-07-28 02:48:51 +0600

    Enhance Intro and Splash Screen layouts, add animation and update color resources

--- app/src/main/java/com/aas/medi_bridge/SplashScreenActivity.kt
+++ app/src/main/java/com/aas/medi_bridge/SplashScreenActivity.kt
@@ -2,28 +2,32 @@ package com.aas.medi_bridge
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import androidx.activity.enableEdgeToEdge
+import android.os.Handler
+import android.os.Looper
+
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.aas.medi_bridge.databinding.ActivitySplashScreenBinding
+import com.google.android.material.animation.AnimationUtils
 
-class SplashScreenActivity : BaseActivity() {
 
-    private lateinit var binding: ActivitySplashScreenBinding
+class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySplashScreenBinding.inflate((layoutInflater))
+        setContentView(R.layout.activity_splash_screen)
+
+        var binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.apply{
-            startBtn.setOnClickListener {
-                startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
-            }
+
+        val animation = android.view.animation.AnimationUtils.loadAnimation(this, R.anim.blink_zoom)
+        binding.splashImageView.startAnimation(animation)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, IntroActivity::class.java))
+            finish()
+        },3000) // 3000 milliseconds =
+
         }
 
-
     }
-}

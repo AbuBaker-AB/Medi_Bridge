98e734e9adf8607bd106dae38a7fa6212eef485a
aktheraqila <aktheraqila@gmail.com>
2025-07-30 01:07:54 +0600

    Refactor IntroActivity: implement ViewBinding, enable edge-to-edge display, and set up button click listener

--- app/src/main/java/com/aas/medi_bridge/IntroActivity.kt
+++ app/src/main/java/com/aas/medi_bridge/IntroActivity.kt
@@ -1,16 +1,25 @@
 package com.aas.medi_bridge
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.aas.medi_bridge.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityIntroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_intro)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            buttonCloud.setOnClickListener {
+                startActivity(Intent(this@IntroActivity, MainActivity::class.java))
+            }
+        }
 
     }
 }

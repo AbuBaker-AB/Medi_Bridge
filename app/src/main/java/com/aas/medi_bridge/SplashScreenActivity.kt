b16a031982188daf8524c84f95af0c09ab1fd8bf
AbuBaker-AB <145045004+AbuBaker-AB@users.noreply.github.com>
2025-08-22 00:39:56 +0600

    Redesign splash and main screens with new layouts
    Refactored SplashScreenActivity to use view binding, added animation, and implemented a timed transition to IntroActivity. Overhauled activity_main.xml with a new UI layout including scrollable content, doctor specialties, top doctors, and a custom bottom navigation bar. Simplified activity_splash_screen.xml to display only the splash image with a new background.

--- app/src/main/java/com/aas/medi_bridge/SplashScreenActivity.kt
+++ app/src/main/java/com/aas/medi_bridge/SplashScreenActivity.kt
@@ -1,2 +1,33 @@
 package com.aas.medi_bridge
 
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+import androidx.appcompat.app.AppCompatActivity
+import com.aas.medi_bridge.databinding.ActivitySplashScreenBinding
+import com.google.android.material.animation.AnimationUtils
+
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        var binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val animation = android.view.animation.AnimationUtils.loadAnimation(this, R.anim.blink_zoom)
+        binding.splashImageView.startAnimation(animation)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, IntroActivity::class.java))
+            finish()
+        },3000) // 3000 milliseconds =
+
+    }
+
+}

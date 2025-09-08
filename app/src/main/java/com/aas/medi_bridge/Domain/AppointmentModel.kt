d4fb6e26f43ce33b896b6fb132eb8530551f6c14
aktheraqila <aktheraqila@gmail.com>
2025-09-09 01:32:46 +0600

    Remove AppointmentModel to resolve redeclaration error; add new vector asset for backspace icon and update colors.xml with red and green color definitions

--- app/src/main/java/com/aas/medi_bridge/Domain/AppointmentModel.kt
+++ app/src/main/java/com/aas/medi_bridge/Domain/AppointmentModel.kt
@@ -1,4 +1,2 @@
 package com.aas.medi_bridge.Domain
-
-// This file is being removed to resolve redeclaration error
-// AppointmentModel is now defined in DoctorModel.kt with all necessary fields
+//
